a9d1a49cf06331deceb7c4e0b0c91234ba4c5adb
Anders Fimreite <602117@stud.hvl.no>
2022-03-21 22:02:14 +0100

    Add department stuff to sql

--- oblig3.sql
+++ oblig3.sql
@@ -10,27 +10,35 @@ CREATE TABLE employee (
   lastname VARCHAR(50) NOT NULL,
   hiredDate DATE,
   position VARCHAR NOT NULL,
-  monthlySalary DECIMAL(10, 2)
-  -- departmentId INTEGER
+  monthlySalary DECIMAL(10, 2),
+  departmentId INTEGER
 );
 
-INSERT INTO employee
-  (username, firstname, lastname, hiredDate, position, monthlySalary)
-VALUES
-  ('andf', 'Anders', 'Fimreite', NOW(), 'Developer', 100000.00),
-  ('jorf', 'Jorgen', 'Fjolstad', NOW(), 'Developer', 100000.00),
-  ('erig', 'Eric', 'Gjerstad', NOW(), 'Developer', 100000.00);
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  leaderId INTEGER,
+  CONSTRAINT fkLeader FOREIGN KEY (leaderId) REFERENCES employee(id) ON DELETE RESTRICT
+);
 
+ALTER TABLE employee
+ADD CONSTRAINT fkDepartment FOREIGN KEY (departmentId) REFERENCES department(id) ON DELETE RESTRICT;
 
--- CREATE TABLE department (
---   id SERIAL PRIMARY KEY,
---   name VARCHAR(50) NOT NULL,
---   leaderId INTEGER,
---   CONSTRAINT fkLeader FOREIGN KEY (leaderId) REFERENCES employee(id) ON DELETE RESTRICT
--- );
+INSERT INTO department (name) VALUES
+  ('Software Development'),
+  ('Accounting'),
+  ('Customer Service');
 
--- ALTER TABLE employee
--- ADD CONSTRAINT fkDepartment FOREIGN KEY (departmentId) REFERENCES department(id) ON DELETE RESTRICT;
+INSERT INTO employee (username, firstname, lastname, hiredDate, position, monthlySalary, departmentId) VALUES
+  ('andf', 'Anders', 'Fimreite', NOW(), 'Developer', 100000.00, 1),
+  ('jorf', 'Jorgen', 'Fjolstad', NOW(), 'Developer', 100000.00, 1),
+  ('erig', 'Eric', 'Gjerstad', NOW(), 'Developer', 100000.00, 1),
+  ('test', 'Test', 'Testesen', NOW(), 'Accountant', 10000.00, 2),
+  ('olan', 'Ola', 'Nordmann', NOW(), 'Scammer', 1000000.00, 3);
+
+UPDATE department SET leaderId = 1 WHERE id = 1;
+UPDATE department SET leaderId = 4 WHERE id = 2;
+UPDATE department SET leaderId = 5 WHERE id = 3;
 
 -- CREATE TABLE project (
 --   id SERIAL PRIMARY KEY,
